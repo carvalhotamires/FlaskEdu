207c1201f9946f7e5cf67b0dd9450543b990b1d4
Tamires Carvalho <tamirescs00@gmail.com>
2025-06-01 19:03:16 -0300

    coloquei os extends de cada pagina para ficar o menu fixo do header

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def home():
     return render_template('inicio.html')
@@ -21,5 +22,9 @@ def fundamentos():
 @app.route('/perguntas')
 def perguntas():
     return render_template('perguntas.html')
+
+@app.route('/header')
+def header():
+    return render_template('header.html')
 
 app.run()
