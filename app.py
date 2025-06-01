1d98aba3c15b03832a65744b79d9d368c22ffd1d
Lucas Aguiar <llucasaguiar16@gmail.com>
2025-06-01 16:39:14 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('inicio.html')
+
+@app.route('/equipe')
+def equipe():
+    return render_template('equipe.html')
+
+@app.route('/dicionario')
+def dicionario():
+    return render_template('dicionario.html')
+
+@app.route('/fundamentos')
+def fundamentos():
+    return render_template('fundamentos.html')
+
+@app.route('/perguntas')
+def perguntas():
+    return render_template('perguntas.html')
+
+app.run()
