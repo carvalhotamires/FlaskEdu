502d6dd12cedf6b6a8427124f82bf707aa3568ab
flaviarhuana <flaviarhuana.quim@gmail.com>
2025-06-01 21:28:11 -0300

    Estruturação do chatbot

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import google.generativeai as genai
+from dotenv import load_dotenv
+import os
 
 app = Flask(__name__)
 
+load_dotenv()
+api_key = os.getenv('GEMINI_API_KEY')
 
-@app.route('/')
+
+@app.route('/', methods=['GET'])
 def home():
     return render_template('inicio.html')
 
@@ -19,8 +25,34 @@ def dicionario():
 def fundamentos():
     return render_template('fundamentos.html')
 
-@app.route('/perguntas')
+
+
+#Config Generative AI
+genai.configure(api_key=api_key)
+model = genai.GenerativeModel(model_name='gemini-2.0-flash')
+
+@app.route('/perguntas', methods=['GET'])
 def perguntas():
     return render_template('perguntas.html')
+
+@app.route('/responder', methods=['POST'])
+def responder():
+    pergunta = request.form['pergunta']
+    resposta = ""
+
+    try:
+        # Envia a pergunta para o modelo Gemini
+        response = model.generate_content(pergunta)
+        resposta = response.text
+    except Exception as e:
+        resposta = f"Erro ao gerar resposta: {str(e)}"
+
+    return render_template('perguntas.html', resposta=resposta)
+
+@app.route('/header')
+def header():
+    return render_template('header.html')
+
+
 
 app.run(debug=True)
