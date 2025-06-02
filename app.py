63fcad7c6d31479d5b13bec72a717500a4d49dff
Lucas Aguiar <llucasaguiar16@gmail.com>
2025-06-02 12:41:34 -0300

    Add files via upload
    Commit changes do app.py e bd_glossario.csv

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for, redirect
 import google.generativeai as genai
 from dotenv import load_dotenv
 import os
+import csv
 
 app = Flask(__name__)
 
 load_dotenv()
 api_key = os.getenv('GEMINI_API_KEY')
+
 
 @app.route('/', methods=['GET'])
 def home():
@@ -16,16 +18,59 @@ def home():
 def equipe():
     return render_template('equipe.html')
 
-@app.route('/dicionario')
-def dicionario():
-    visualizar_termos(bd_termos)  # Só para fins de log
-    return render_template('dicionario.html', termos=bd_termos)
+@app.route('/glossario')
+def glossario():
+
+    glossario_de_termos = []
+
+    with open('bd_glossario.csv', newline='', encoding='utf-8') as csvfile:
+        reader = csv.reader(csvfile, delimiter=';')
+
+        for t in reader:
+            glossario_de_termos.append(t)
+
+    return render_template('glossario.html', glossario=glossario_de_termos)
+
+
+@app.route('/novo_termo')
+def novo_termo():
+    return render_template('novo_termo.html')
+
+@app.route('/criar_termo', methods=['POST'])
+def criar_termo():
+
+    termo = request.form['termo']
+    definicao = request.form['definicao']
+
+    with open('bd_glossario.csv', 'a', newline='', encoding='utf-8') as csvfile:
+        writer = csv.writer(csvfile, delimiter=';')
+        writer.writerow([termo, definicao])
+
+    return redirect(url_for('glossario'))
+
+@app.route('/excluir_termo/<int:termo_id>', methods=['POST'])
+def excluir_termo(termo_id):
+
+    with open('bd_glossario.csv', 'r', newline='') as file:
+        reader = csv.reader(file)
+        linhas = list(reader)
+
+    for i, linha in enumerate(linhas):
+        if i == termo_id:
+            del linhas[i]
+            break
+
+    with open('bd_glossario.csv', 'w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerows(linhas)
+
+    return redirect(url_for('glossario'))
 
 @app.route('/fundamentos')
 def fundamentos():
     return render_template('fundamentos.html')
 
-# Config GenerativeAI
+#Config Generative AI
 genai.configure(api_key=api_key)
 model = genai.GenerativeModel(model_name='gemini-2.0-flash')
 
@@ -33,21 +78,19 @@ model = genai.GenerativeModel(model_name='gemini-2.0-flash')
 def perguntas():
     return render_template('perguntas.html')
 
-
 @app.route('/responder', methods=['POST'])
 def responder():
     pergunta = request.form['pergunta']
     resposta = ""
 
     try:
-        #Envia a pergunta para o modelo Gemini
+        # Envia a pergunta para o modelo Gemini
         response = model.generate_content(pergunta)
         resposta = response.text
     except Exception as e:
         resposta = f"Erro ao gerar resposta: {str(e)}"
 
     return render_template('perguntas.html', resposta=resposta)
-
 
 @app.route('/header')
 def header():
